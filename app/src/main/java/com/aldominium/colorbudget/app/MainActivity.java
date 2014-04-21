58c43094441f124c7f01e87a9ccfde3d44b174de
aldo <aldo2122@outlook.com>
2014-04-21 17:42:31 -0500

    funciona
    funciona

--- app/src/main/java/com/aldominium/colorbudget/app/MainActivity.java
+++ app/src/main/java/com/aldominium/colorbudget/app/MainActivity.java
@@ -1,13 +1,19 @@
 package com.aldominium.colorbudget.app;
 
 import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
@@ -26,7 +32,7 @@ import java.util.HashMap;
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
 
     /**
@@ -40,6 +46,14 @@ public class MainActivity extends Activity {
     protected int mSelectedMonth;
     protected int mSelectedDay;
     protected List<ParseObject> mPaymentNames;
+    protected AlarmManager mAlarmManager;
+    protected Intent mNotificationServiceIntent;
+    protected PendingIntent mNotificationServicePendingIntent;
+
+    private static final long INITIAL_ALARM_DELAY = 2 * 60 * 1000L;
+    protected static final long JITTER = 5000L;
+
+
 
 
 
@@ -50,6 +64,26 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_main);
         mCalendar = (CalendarView)findViewById(R.id.calendarView);
+
+        mAlarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        Log.e(TAG, "Creo la alarma");
+        //Crea pending intent para las alarmas
+        mNotificationServiceIntent = new Intent(MainActivity.this,
+                NotificationService.class);
+        mNotificationServicePendingIntent = PendingIntent.getService(
+                MainActivity.this, 0, mNotificationServiceIntent, 0);
+
+        mAlarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime() + INITIAL_ALARM_DELAY,
+                AlarmManager.INTERVAL_FIFTEEN_MINUTES,
+                mNotificationServicePendingIntent);
+
+
+        //Servicio para checar por notificaciones
+        //Intent serviceIntent = new Intent(MainActivity.this,NotificationService.class);
+
+
 
         //Checa si el usuario esta logeado,sino lo saca
         final ParseUser currentUser = ParseUser.getCurrentUser();
@@ -67,9 +101,9 @@ public class MainActivity extends Activity {
         mSelectedMonth = calendar.get(Calendar.MONTH)+1;
         mSelectedYear = calendar.get(Calendar.YEAR);
 
-        Toast.makeText(getBaseContext(),"Selected Date is\n\n"
+        /*Toast.makeText(getBaseContext(),"Selected Date is\n\n"
                         +mSelectedDay+" : "+(mSelectedMonth)+" : "+mSelectedYear ,
-                Toast.LENGTH_LONG).show();
+                Toast.LENGTH_LONG).show();*/
 
 
         mCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
@@ -110,7 +144,7 @@ public class MainActivity extends Activity {
                             final ListView listview = (ListView) findViewById(R.id.listView);
 
                             final StableArrayAdapter adapter = new StableArrayAdapter(MainActivity.this,
-                                    android.R.layout.simple_list_item_1, list);
+                                    android.R.layout.simple_list_item_checked, list);
                             listview.setAdapter(adapter);
 
 
@@ -146,9 +180,9 @@ public class MainActivity extends Activity {
 
         super.onResume();
 
-        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Payments");
-        query.whereEqualTo("userId",ParseUser.getCurrentUser().getObjectId());
-        query.whereEqualTo("day",mSelectedDay);
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(ParseConstants.CLASS_PAYMENTS);
+        query.whereEqualTo(ParseConstants.KEY_USER_ID,ParseUser.getCurrentUser().getObjectId());
+        query.whereEqualTo(ParseConstants.KEY_DAY,mSelectedDay);
         query.setLimit(1000);
         query.findInBackground(new FindCallback<ParseObject>() {
 
@@ -173,7 +207,7 @@ public class MainActivity extends Activity {
                     final ListView listview = (ListView) findViewById(R.id.listView);
 
                     final StableArrayAdapter adapter = new StableArrayAdapter(MainActivity.this,
-                            android.R.layout.simple_list_item_1, list);
+                            android.R.layout.simple_list_item_checked, list);
                     listview.setAdapter(adapter);
 
 
@@ -285,6 +319,28 @@ public class MainActivity extends Activity {
     }
 
 
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id)
+    {
+
+
+        super.onListItemClick(l, v, position, id);
+        if (getListView().getCheckedItemCount() > 0){
+            //show the delete button
+        }else {
+            //remove the delete button
+        }
+    }
+
+    protected void removeCheckedItems(){
+        for(int i = 0;i <getListView().getCount();i++){
+            getListView().getItemAtPosition(i);
+            if (getListView().isItemChecked(i)){
+                //remove item
+            }
+        }
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
@@ -320,7 +376,7 @@ public class MainActivity extends Activity {
                     final ListView listview = (ListView) findViewById(R.id.listView);
 
                     final StableArrayAdapter adapter = new StableArrayAdapter(MainActivity.this,
-                            android.R.layout.simple_list_item_1, list);
+                            android.R.layout.simple_list_item_checked, list);
                     listview.setAdapter(adapter);
 
 
@@ -339,4 +395,6 @@ public class MainActivity extends Activity {
         });
 
     }
+
+
 }
---
+++ app/src/main/java/com/aldominium/colorbudget/app/NotificationService.java
@@ -0,0 +1,101 @@
+package com.aldominium.colorbudget.app;
+
+import android.app.AlertDialog;
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.util.Log;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.Calendar;
+import java.util.List;
+
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions and extra parameters.
+ */
+public class NotificationService extends IntentService {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    public int mActualDay;
+    public int mActualMonth;
+    public int mActualYear;
+
+    // Notification Action Elements
+    private Intent mNotificationIntent;
+    private PendingIntent mContentIntent;
+
+    // Notification Text Elements
+    private final CharSequence tickerText = "Are You Playing Angry Birds Again!";
+    private final CharSequence contentTitle = "A Kind Reminder";
+    private final CharSequence contentText = "Get back to studying!!";
+
+    //We must call the super constructor
+    public NotificationService() {
+        super("NotificationService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            Calendar calendar = Calendar.getInstance();
+            mActualDay = calendar.get(Calendar.DAY_OF_MONTH);
+            mActualMonth = calendar.get(Calendar.MONTH)+1;
+            mActualYear = calendar.get(Calendar.YEAR);
+
+            ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(ParseConstants.CLASS_PAYMENTS);
+            query.whereEqualTo(ParseConstants.KEY_USER_ID, ParseUser.getCurrentUser().getObjectId());
+            query.whereEqualTo(ParseConstants.KEY_DAY, mActualDay +1);
+            query.whereEqualTo(ParseConstants.KEY_MONTH, mActualMonth);
+            query.whereEqualTo(ParseConstants.KEY_YEAR, mActualYear);
+            query.setLimit(1000);
+            query.findInBackground(new FindCallback<ParseObject>() {
+
+                @Override
+                public void done(List<ParseObject> parseObjects, ParseException e)
+                {
+                    if (e == null){
+                        //objects retrieved succesfully
+                        if (parseObjects.isEmpty()){
+                            //There are no payments due, yay!!
+                            Log.e(TAG, "No payments");
+                        }else {
+                            //We have payments, send notification
+                            Log.e(TAG, "payments");
+                            mNotificationIntent = new Intent(NotificationService.this,MainActivity.class);
+                            mContentIntent = PendingIntent.getActivity(NotificationService.this, 0,
+                                    mNotificationIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                            Notification.Builder notificationBuilder = new Notification.Builder(
+                                    NotificationService.this).setTicker(tickerText)
+                                    .setSmallIcon(android.R.drawable.stat_sys_warning)
+                                    .setAutoCancel(true).setContentTitle(contentTitle)
+                                    .setContentText(contentText).setContentIntent(mContentIntent);
+
+                            NotificationManager mNotificationManager = (NotificationManager) NotificationService.this
+                                    .getSystemService(NotificationService.this.NOTIFICATION_SERVICE);
+                            mNotificationManager.notify(1,
+                                    notificationBuilder.build());
+                        }
+                    }else{
+                        Log.e(TAG, e.getMessage());
+                        //Error in object retrieval
+
+                    }
+
+                }
+            });
+        }
+    }
+
+}
